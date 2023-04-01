c0df314adcd56e8ab35ecaa8458563ccd9473412
Phakin Neamson <phakin.neamson@gmail.com>
2023-04-02 02:19:22 +0700

    cramer not done

--- src/pages/Linear Agebra/Cramer.tsx
+++ src/pages/Linear Agebra/Cramer.tsx
@@ -1,54 +1,75 @@
-import { NumberInput , Group , Button, Grid , Card , Container , Flex} from '@mantine/core'
-import React , { Fragment, useState } from 'react'
+import { NumberInput , Group , Button, Grid , Container , NativeSelect} from '@mantine/core'
+import { IconChevronDown } from '@tabler/icons-react';
+import React , { useState } from 'react'
 import Header from '../../components/Header'
 
 function Cramer() {
-    const [NumberMatrix,setNumberMatrix] = useState(0)
+    const [NumberMatrix,setNumberMatrix] = useState('2')
+    const [Matrix,setMatrix] = useState<number[][]>(
+        Array(Number(NumberMatrix))
+        .fill(0)
+        .map(() => Array(Number(NumberMatrix)).fill(0))
+    )
+    const [ValueMatrix,setValueMatrix] = useState<[]>([])
+   
 
-    const createMatrix = (e:number) =>{
-        let count = 1;
+    const createMatrix = (e:string) =>{
+        let Dimension = Number(e)
+        let ansColumn = Dimension+1
         const input = []
-        const ansInput = []
-        if(e>6 || e === 1){
-            e=0
-        }
-        else{
-            for(let i=0;i<e;i++){
-                for(let j=0;j<e;j++){
-                    input.push(
-                        <Grid.Col  key={count} span={12/e}>
-                            <NumberInput label={count} value={0} required/>
-                        </Grid.Col>)
-                    count++;
-                }
+        
+        for(let i=0;i<Dimension;i++){
+            for(let j=0;j<Dimension;j++){
+                input.push(
+                    <Grid.Col key={`${i}-${j}`} span={(ansColumn*2)/ansColumn}>
+                        <NumberInput label={"x"} value={0} onChange={(event)=>setValueOfMatrix(event,i,j)} required hideControls/>
+                    </Grid.Col>)
             }
+            input.push(
+                <Grid.Col key={i} span={(ansColumn*2)/ansColumn}>
+                    <NumberInput label={"y"} value={0} required hideControls/>
+                </Grid.Col>)
         }
+        
         return(
-            <Grid columns={12%e === 0?12:5}>
+            <Grid columns={ansColumn*2}>
                 {input}
             </Grid>
         )
     }
+    
+    const setValueOfMatrix = (value:number|undefined,i:number,j:number)=>{
+        if(value !== undefined){
+            const matrix:any = [...Matrix]
+            matrix[i][j] = value
+            console.log(matrix);
+            //setMatrix(matrix)
+        }
+       
+    }
 
     const CalCramer = (e:React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
-        console.log(e);
+        console.log(e.currentTarget);
     }
 
-    const setNumMatrix = (e:number)=>{
-        setNumberMatrix(e)
-    }
   return (
     <>
         <Header text="Cramer"/>
-         <Container size="xs" px="md">
+         <Container size={550} px="md">
             <form onSubmit={CalCramer}>
-                <NumberInput
-                    width={100}
+                <NativeSelect
                     mb={20}
-                    label="Dimension Of Matrix"
-                    onChange={setNumMatrix}
-                    required
+                    data={['2','3','4','5','6','7','8']}
+                    rightSection={<IconChevronDown size="1rem" />}
+                    rightSectionWidth={40}
+                    label="Dimension Of Matrix 2-8"
+                    onChange={(event) => {setMatrix(Array(Number(event.currentTarget.value))
+                        .fill(0)
+                        .map(() => Array(Number(event.currentTarget.value)).fill(0)))
+                        setNumberMatrix(event.currentTarget.value)
+                        }
+                    }
                 />
                 {createMatrix(NumberMatrix)}
                 <Group position='center' mb="md">
