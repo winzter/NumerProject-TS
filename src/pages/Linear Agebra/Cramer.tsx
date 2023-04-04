3ad1b8944565b95404455e5ee8cb53586eeb32bf
Phakin Neamson <92076305+winzter@users.noreply.github.com>
2023-04-04 21:46:22 +0700

    zzz

--- src/pages/Linear Agebra/Cramer.tsx
+++ src/pages/Linear Agebra/Cramer.tsx
@@ -1,7 +1,7 @@
-import React , { useState } from 'react'
-import { Group , Button , Container , NativeSelect} from '@mantine/core'
-import { IconChevronDown } from '@tabler/icons-react';
+import React , { JSXElementConstructor, useState } from 'react'
+import { Group , Button , Container } from '@mantine/core'
 import { det } from 'mathjs'
+import InputMatrix from '../../components/InputMatrix';
 import CreateMatrix from '../../components/CreateMatrix';
 import Header from '../../components/Header'
 
@@ -15,7 +15,10 @@ function Cramer() {
     const [ValueMatrix,setValueMatrix] = useState<number[]>(
         Array(Number(NumberMatrix)).fill(0)
     )
-    const [Answer,setAnswer] = useState<number[]>([])
+    const [Answer,setAnswer] = useState<number[]>(
+        Array(NumberMatrix).fill(0)
+    )
+    
 
     const setValueOfMatrix = (value:number,i:number,j:number)=>{
         if(value !== undefined){
@@ -36,66 +39,102 @@ function Cramer() {
         }
     }
 
+    const showAns = ()=>{
+        let ans:React.ReactNode[] = []
+        for(let i=0;i<NumberMatrix;i++){
+            ans.push(
+                <h2 key={i}>Y{i} = {Answer[i]}</h2>
+            )
+        }
+        return(
+            <div>
+                <h1>asdasdasd</h1>
+            </div>
+        )
+    }
+
+    const setMatrixState = (value:number)=>{
+        console.log(value);
+        setMatrix(
+            Array(value)
+            .fill(0)
+            .map(() => 
+                Array(value).fill(0)
+            )
+        )
+        setValueMatrix(Array(value).fill(0))
+        setNumberMatrix(value)
+    }
+
     const CalCramer = (e:React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault()
         console.log(Matrix);
         console.log(ValueMatrix);
         
-        let k=0 , mC:number[][],x:number[] = Array(NumberMatrix).fill(0),mB:number[] = [...ValueMatrix],mA:number[][] = [...Matrix]
-        for(let j=0;j<NumberMatrix;j++){
-            mC = mA.map(row => [...row])
-            for(let i=0;i<NumberMatrix;i++){
-                mC[i][k] = mB[i];
-            }
-            x[k] = det(mC)/det(mA);
-            console.log(`X${k+1} = ${x[k]}`);
-            k++;
+        let k=0 , 
+        mC:number[][],
+        ans:number[]=[],
+        x:number[] = Array(NumberMatrix).fill(0),
+        mB:number[] = [...ValueMatrix],
+        mA:number[][] = [...Matrix],
+        DetA:number = det(mA)
+        if(DetA === 0){
+            console.log("Can't divide by zero!");
         }
+        else{
+            for(let j=0;j<NumberMatrix;j++){
+                mC = mA.map(row => [...row])
+                for(let i=0;i<NumberMatrix;i++){
+                    mC[i][k] = mB[i];
+                }
+                x[k] = det(mC)/DetA;
+                ans.push(x[k])
+                console.log(`Y${k} = ${x[k]}`);
+                k++;
+            }
+            console.log(ans);
+            setAnswer(ans)
+        } 
     }
+    
 
   return (
     <>
         <Header text="Cramer"/>
-         <Container size={550} px="md">
-            <form onSubmit={CalCramer}>
-                <NativeSelect
-                    mb={20}
-                    data={['2','3','4','5','6','7','8']}
-                    rightSection={<IconChevronDown size="1rem" />}
-                    rightSectionWidth={40}
-                    label="Dimension Of Matrix 2-8"
-                    onChange={(event) => {
-                        setMatrix(
-                            Array(Number(event.currentTarget.value))
-                            .fill(0)
-                            .map(() => 
-                                Array(Number(event.currentTarget.value)).fill(0)
-                            )
-                        )
-                        setValueMatrix(Array(Number(event.currentTarget.value)).fill(0))
-                        setNumberMatrix(Number(event.currentTarget.value))
-                        }
-                    }
-                />
-                <CreateMatrix 
-                    Dimension={NumberMatrix} 
-                    setValueOfMatrix={setValueOfMatrix}
-                    setAnsOfMatrix={setAnsOfMatrix}
-                    MatrixData={Matrix}
-                    AnsData={ValueMatrix}
-                />
-                <Group position='center' mb="md">
-                    <Button 
-                        mt="md" 
-                        size='sm' 
-                        type='submit' 
-                        variant="gradient"
-                        gradient={{ from: 'pink', to: 'orange', deg:60 }}>
-                            Calculate
-                    </Button>
-                </Group>
-            </form>
-        </Container>
+        {/* <Group position="center">
+        <Grid justify='center'>
+            <Grid.Col span="content">*/}
+                {/* <Card withBorder radius='md' p='xs' shadow='md'>  */}
+                    <Container size={550} px="md">
+                        <form onSubmit={CalCramer}>
+                            <InputMatrix
+                                setMarixState={setMatrixState}
+                            />
+                            <CreateMatrix 
+                                Dimension={NumberMatrix} 
+                                setValueOfMatrix={setValueOfMatrix}
+                                setAnsOfMatrix={setAnsOfMatrix}
+                                MatrixData={Matrix}
+                                AnsData={ValueMatrix}
+                            />
+                            <Group position='center' mb="md">
+                                <Button 
+                                    mt="md" 
+                                    size='sm' 
+                                    type='submit' 
+                                    variant="gradient"
+                                    gradient={{ from: 'pink', to: 'orange', deg:60 }}>
+                                        Calculate
+                                </Button>
+                            </Group>
+                        </form>
+                        
+                    </Container>
+                    {/* {showAns} */}
+                 {/* </Card> */}
+            {/*</Grid.Col>
+        </Grid>
+        </Group> */}
     </>
   )
 }
